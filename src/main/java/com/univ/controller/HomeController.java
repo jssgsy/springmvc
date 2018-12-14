ce3b747f97f78e3e84246ad8496bcc90a8661bb5
univ <fcsnwu@163.com>
2018-12-14 18:07:22 +0800

    返回json数据

--- src/main/java/com/univ/controller/HomeController.java
+++ src/main/java/com/univ/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.univ.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.univ.domain.Demo;
 
 /**
  * @author univ
@@ -66,5 +72,37 @@ public class HomeController {
 
         // 在业务中抛出了异常
         throw new Exception("HomeController: 抛出异常Exception");
+    }
+
+    /**
+     * 需要引入jackson-databind包，spring-webmvc没有默认依赖
+     * 返回json格式，可能哪里少了
+     * @return
+     */
+    @RequestMapping("/json")
+    @ResponseBody
+    public List second() {
+        List list = new ArrayList();
+
+        List l2 = new ArrayList();
+        Demo d1 = new Demo();
+        Demo d2 = new Demo();
+        Demo d3 = new Demo();
+        l2.add(d1);
+        l2.add(d2);
+        l2.add(d3);
+
+        List l3 = new ArrayList();
+        Demo d4 = new Demo();
+        Demo d5 = new Demo();
+        Demo d6 = new Demo();
+        l3.add(d4);
+        l3.add(d5);
+        l3.add(d6);
+
+
+        list.add(l2);
+        list.add(l3);
+        return list;
     }
 }
