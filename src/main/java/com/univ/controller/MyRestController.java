3a0e8e8fbd48eb49b4d94bff56b856257ccf5933
univ <fcsnwu@163.com>
2018-12-14 18:55:26 +0800

    @RestController

---
+++ src/main/java/com/univ/controller/MyRestController.java
@@ -0,0 +1,48 @@
+package com.univ.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.univ.domain.Demo;
+
+/**
+ * @author univ
+ * @datetime 2018/12/14 6:50 PM
+ * @description
+ */
+
+/*
+@RestController与@Controller的区别
+@RestController相当于其中的所有方法都被@ResponseBody所注解，此时可以不用显示书写@ResponseBody
+当然，此时也没法返回视图，就只能纯粹返回json数据了
+ */
+@RestController
+@RequestMapping("myrest")
+public class MyRestController {
+
+    /**
+     * 这里不用显示的@ResponseBody，直接返回json数据
+     * @return
+     */
+    @RequestMapping("/demos")
+    public List<Demo> getDemos() {
+        Demo d1 = new Demo();
+        d1.setAge(10);
+        d1.setName("aaa");
+        Demo d2 = new Demo();
+        d2.setAge(20);
+        d2.setName("bbb");
+
+        Demo d3 = new Demo();
+        d3.setAge(20);
+        d3.setName("ccc");
+
+        Demo d4 = new Demo();
+        d4.setAge(40);
+        d4.setName("ddd");
+        return Arrays.asList(d1, d2, d3, d4);
+    }
+}
