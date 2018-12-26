82f4a5b42ecbb4a89d2d77f8850c3515dfdda345
univ <fcsnwu@163.com>
2018-12-26 16:52:05 +0800

    定时任务异步化

--- src/main/java/com/univ/task/TaskDemo.java
+++ src/main/java/com/univ/task/TaskDemo.java
@@ -1,5 +1,6 @@
 package com.univ.task;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -34,6 +35,18 @@ public class TaskDemo {
     }
     //-------------------------任务默认是串行执行的，上面三个任务会是同一个线程执行，且执行完一个再执行下一个------------------------
 
+    @Scheduled(cron = "0/5 * * * * *")
+    @Async
+    public void sayHello4() {
+        System.out.println("sayHello4当前线程：" + Thread.currentThread().getName());
+    }
+
+    @Scheduled(cron = "0/5 * * * * *")
+    @Async
+    public void sayHello5() {
+        System.out.println("sayHello5当前线程：" + Thread.currentThread().getName());
+    }
+    //---------------加上@Async注解后，任务便是异步的了，即每个任务每次执行时都会启动不同的线程来执行-----------------------
 
 
 
