9cdd357ebe7229b13e855f2ba1711819fe663f05
univ <fcsnwu@163.com>
2023-10-24 11:39:21 +0800

    原生集成mybatis

---
+++ src/main/java/com/univ/controller/DBController.java
@@ -0,0 +1,26 @@
+package com.univ.controller;
+
+import com.univ.domain.Single;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author univ
+ * date 2023/10/24
+ */
+@RestController
+@RequestMapping("/db")
+public class DBController {
+
+    @Resource
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    @RequestMapping("/get")
+    public List<Single> get() {
+        return sqlSessionTemplate.selectList("SingleMapper.getById", 2L);
+    }
+}
